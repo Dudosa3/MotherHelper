be992346736127d7a3f031ce683f16e5b80933ef
Arsenii Novosad <holanovo@gmail.com>
2022-06-01 22:09:15 +0200

    Map Fixed

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
-let map;
 
 function initMap() {
+
+    //map 
     
-    var options = {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8,
+    
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 6.3,
+      });
+
+    map.setCenter(new google.maps.LatLng(49.8, 15.5))
+
+    //marke
+   
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(usersPosition);
+      } else { 
+        const marker = new google.maps.Marker({
+            position:{lat: 50.0755, lng: 14.4378 },
+            map:map,
+        });
+      }
     }
-    map = new google.maps.Map(document.getElementById("map"), options) 
-  }
+    
+
+
+    
+function currentPosition(position){
+
+    const marker = new google.maps.Marker({
+        position:{lat: position.coords.latitude, lng: position.coords.longitude},
+        map:map,
+    });
+}
+ 
   
-  window.initMap = initMap;
+
