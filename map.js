a26c4d4008d75d28d35753dee9ec2a6bc0f95228
Arsenii Novosad <holanovo@gmail.com>
2022-06-13 23:53:34 +0200

    get coordinates

--- map.js
+++ map.js
@@ -1,8 +1,39 @@
+const defaultPosition = {
+  lat: 49.8,
+  lng: 15.5,
+  zoom: 6.3
+}
+
+let position= {
+  lat: null,
+  lng: null
+}
+
+
+  const GeolocateSuccess = (position) => {
+
+    console.log(position);
+
+  };
+  
+  navigator.geolocation.getCurrentPosition(GeolocateSuccess, GeolocateError);
+    
+
+
+ 
+
+    function GeolocateError(error) {  console.warn(error.code, error.message);   
+      if (error.code === 1) {    
+        // they said no  
+      } else if (error.code === 2) {   
+        // position unavailable  
+        } else if (error.code === 3) {    
+          // timeout  
+        }}
 
 function initMap() {
 
     //map 
-    
     
     map = new google.maps.Map(document.getElementById("map"), {
         zoom: 6.3,
@@ -10,17 +41,15 @@ function initMap() {
 
     map.setCenter(new google.maps.LatLng(49.8, 15.5))
 
-    //marke
+    //marker
    
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(usersPosition);
-      } else { 
+   
         const marker = new google.maps.Marker({
             position:{lat: 50.0755, lng: 14.4378 },
             map:map,
         });
       }
-    }
+     
     
 
 
@@ -28,7 +57,7 @@ function initMap() {
 function currentPosition(position){
 
     const marker = new google.maps.Marker({
-        position:{lat: position.coords.latitude, lng: position.coords.longitude},
+        position:{lat: lat, lng: lng},
         map:map,
     });
 }
