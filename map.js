423c8c6f4823639918937ba0204d026d38b61115
Arsenii Novosad <holanovo@gmail.com>
2022-06-14 02:20:25 +0200

    Getting latitude and longitude from cookies

--- map.js
+++ map.js
@@ -1,18 +1,25 @@
-const defaultPosition = {
+const defaultLocation = {
   lat: 49.8,
   lng: 15.5,
   zoom: 6.3
 }
 
-let position= {
+let userLocation= {
   lat: null,
-  lng: null
+  lng: null,
+  zoom: 10
 }
+var map;
+var marker;
 
 
   const GeolocateSuccess = (position) => {
 
-    console.log(position);
+
+    userLocation.lat = parseFloat(position.coords.latitude);
+    userLocation.lng = parseFloat(position.coords.longitude);
+
+    userLocation.lng
 
   };
   
@@ -22,7 +29,7 @@ let position= {
 
  
 
-    function GeolocateError(error) {  console.warn(error.code, error.message);   
+    function GeolocateError (error){console.warn(error.code, error.message);   
       if (error.code === 1) {    
         // they said no  
       } else if (error.code === 2) {   
@@ -31,36 +38,44 @@ let position= {
           // timeout  
         }}
 
+function getLocation()
+{
+  marker.setMap(null)
+  map.setCenter(new google.maps.LatLng(Number(userLocation.lat), Number(userLocation.lng)))
+  map.setZoom(14)
+  marker = new google.maps.Marker({
+    position:{lat: Number(userLocation.lat), lng: Number(userLocation.lng )},
+    map:map,
+});
+
+}
+
 function initMap() {
 
     //map 
     
     map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 6.3,
+        zoom: defaultLocation.zoom,
       });
 
-    map.setCenter(new google.maps.LatLng(49.8, 15.5))
+    map.setCenter(new google.maps.LatLng(Number(defaultLocation.lat), Number(defaultLocation.lng)))
 
     //marker
    
    
-        const marker = new google.maps.Marker({
+        marker = new google.maps.Marker({
             position:{lat: 50.0755, lng: 14.4378 },
             map:map,
         });
+
+        
+
       }
      
     
 
 
     
-function currentPosition(position){
 
-    const marker = new google.maps.Marker({
-        position:{lat: lat, lng: lng},
-        map:map,
-    });
-}
- 
   
 
